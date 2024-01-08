0a71d5336da22abfc353fd4e7f6cc47a52685b3d
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-01-08 10:33:51 +0600

    vite, router dom, tailwind and route setup

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Login from './Pages/User/Login';
+import Register from './Pages/User/Register';
+import User from './Pages/User/User';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/user",
+    element: <User />,
+    children: [
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      }
+    ]
+  }
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
