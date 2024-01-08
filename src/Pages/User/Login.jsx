0c75efd819ba269a23224ea27e2df708f67b31df
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-01-08 11:01:07 +0600

    login page design

--- src/Pages/User/Login.jsx
+++ src/Pages/User/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiLoginCircleLine } from 'react-icons/ri'; // Using React Icons for login button
+import { HiOutlineMail } from "react-icons/hi";
+import { IoKeyOutline } from "react-icons/io5";
+import { FaRegFaceRollingEyes } from "react-icons/fa6";
+import { PiSmileyXEyes } from "react-icons/pi";
+import { FaFacebookF, FaGoogle } from "react-icons/fa";
+import { useOutletContext } from 'react-router-dom';
 
 const Login = () => {
+    const [isPassword, setIsPassword] = useOutletContext();
+
+
     return (
         <div>
-            login
+            <h2 className="text-2xl font-cinzel font-extrabold uppercase mb-4">
+                Welcome Back
+            </h2>
+            <form>
+                <div className="mb-4 flex items-center border rounded-md p-2">
+                    <HiOutlineMail className="mr-2" />
+                    <input type="email" placeholder="Email" className="flex-1 outline-none" />
+                </div>
+
+                <div className="mb-4 flex items-center border rounded-md p-2">
+                    <IoKeyOutline className="mr-2" />
+                    <input type={isPassword ? "password" : "text"} placeholder="Password" className="flex-1 outline-none" />
+                    {
+                        isPassword ?
+                            <FaRegFaceRollingEyes onClick={() => setIsPassword(!isPassword)} className="ms-2" />
+                            :
+                            <PiSmileyXEyes onClick={() => setIsPassword(!isPassword)} className="ms-2" size={19} />
+                    }
+                </div>
+
+                <button type="submit" className="mb-4 duration-300 bg-green-600 hover:bg-green-700 text-white p-2 rounded-md flex items-center justify-center w-full font-bold">
+                    <RiLoginCircleLine className="mr-2" />
+                    Login
+                </button>
+            </form>
+
+            {/* or devider */}
+            <div className='mb-4 flex items-center justify-between gap-4'>
+                <hr className='border border-gray-200 border-dashed	 w-[35%] ' />
+                <p className='uppercase '>or login with</p>
+                <hr className='border border-gray-200 border-dashed	 w-[35%] ' />
+            </div>
+
+            {/* google and facebook login */}
+            <div className='flex justify-between gap-8'>
+                <button className='duration-300 grow py-3 rounded-md border border-[#EA4335] hover:bg-[#EA4335] text-[#EA4335] hover:text-white flex items-center justify-center'>
+                    <FaGoogle />
+                </button>
+                <button className='duration-300 grow py-3 rounded-md border border-[#316FF6] hover:bg-[#316FF6] text-[#316FF6] hover:text-white flex items-center justify-center'>
+                    <FaFacebookF />
+                </button>
+            </div>
         </div>
     );
 };
--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import loginImg from '../../assets/login/login.jpg'
+import loginBG from '../../assets/login/loginBg.jpg'
+import { FaCircleUser } from "react-icons/fa6";
+import { BiSolidMessageSquareEdit } from "react-icons/bi";
+// import LoginForm from './LoginForm';
 
 const User = () => {
+    const navigate = useNavigate();
+    const [isPassword, setIsPassword] = useState(true);
+    const [toggle, setToggle] = useState(1);
+    
+
+    const handleLogin = () => {
+        navigate('/user/login');
+        setToggle(1)
+    }
+    const handleRegister = () => {
+        navigate('/user/register');
+        setToggle(2)
+    }
+
     return (
-        <div>
-            
-            <Outlet />
+        <div className='flex items-center justify-center h-screen bg-cover bg-center' style={{ backgroundImage: `linear-gradient(to bottom, #2aec489c, #d1a05487), url("${loginBG}")` }}>
+            <div className="min-h-[406px] bg-red-500 flex " >
+                {/* First div with Image */}
+                <div className="w-[250px]">
+                    <img src={loginImg} alt="Resturent app login" className="object-cover w-full h-full" />
+                </div>
+
+                {/* Second div with login, register, and forgot password icons */}
+                <div className="flex flex-col justify-between w-[250px] border-r-2 border-gray-100	">
+                    <div onClick={() => handleLogin()} className={`flex flex-col items-center justify-center gap-2 capitalize grow px-[40px] cursor-pointer duration-500 ${toggle === 1 ? 'bg-green-600 text-white' : "bg-white"}`}>
+                        <FaCircleUser size={24} />
+                        <span>Login</span>
+                    </div>
+
+                    <div onClick={() => handleRegister()} className={`flex flex-col items-center justify-center gap-2 capitalize grow px-[40px] cursor-pointer duration-500 ${toggle === 2 ? 'bg-green-600 text-white' : "bg-white"}`}>
+                        {/* Adjust the path and size as needed */}
+                        <BiSolidMessageSquareEdit size={24} />
+                        <span>Register</span>
+                    </div>
+                </div>
+
+                {/* Third div for login, register, and forgot form */}
+                <div className="bg-white p-8 w-[600px] text-center">
+                    {/* for login */}
+                    <div className={toggle === 1 ? "block" : "hidden"}>
+                        <Outlet context={[isPassword, setIsPassword]} />
+                    </div>
+
+                    {/* todo: for register */}
+                    <div className={toggle === 2 ? "block" : "hidden"}>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
